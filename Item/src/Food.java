7b6b8584f47af45b696d5b56270012cdea4e198f
753621 <753621@2452IR306093WYD.ad1.peelsb.com>
2025-12-05 08:42:45 -0500

    Food class items

--- Item/src/Food.java
+++ Item/src/Food.java
@@ -1,4 +1,29 @@
+public class Food extends item {
 
-public class Food {
 
+private double staminaRestore;
+private String spawnRule;
+
+
+public Food(String itemName, double staminaRestore, String spawnRule) {
+super(itemName);
+this.staminaRestore = staminaRestore;
+this.spawnRule = spawnRule;
+}
+
+public double getStaminaRestore() {
+return staminaRestore;
+}
+
+public void setStaminaRestore(double staminaRestore) {
+this.staminaRestore = staminaRestore;
+}
+
+public String getSpawnRule() {
+return spawnRule;
+}
+
+public void setSpawnRule(String spawnRule) {
+this.spawnRule = spawnRule;
+}
 }
