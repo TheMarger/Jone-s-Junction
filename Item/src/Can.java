7acb84f7e073332f86453871b34cd4c5f4948263
Jeeva <Jeeva@10.0.0.234>
2025-12-09 14:26:15 -0500

    subclasses of throwables

--- Item/src/Can.java
+++ Item/src/Can.java
@@ -1,4 +1,11 @@
 
-public class Can {
+public class Can extends Throwable {
 
+    private static final int DefaultThrowRadius = 5;
+    private static final int DefaultSoundValue = 5;
+    private static final int DefaultSoundArea = 10;
+
+    public Can() {
+        super("Can", DefaultThrowRadius, DefaultSoundValue, DefaultSoundArea);
+    }
 }
--- Item/src/Pebble.java
+++ Item/src/Pebble.java
@@ -1,4 +1,11 @@
 
-public class Pebble {
+public class Pebble extends Throwable {
 
+	private static final int DefaultThrowRadius = 10;
+	private static final int DefaultSoundValue = 3;
+	private static final int DefaultSoundArea = 5;
+
+	public Pebble() {
+		super("Pebble", DefaultThrowRadius, DefaultSoundValue, DefaultSoundArea);
+	}
 }
