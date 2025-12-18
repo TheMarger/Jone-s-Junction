a723a7f11930d4440846a399ccd81dc37eb58141
marge <marge@10.71.206.69>
2025-12-18 09:52:44 -0500

    loop

--- GameLoop/src/main/Main.java
+++ GameLoop/src/main/Main.java
@@ -1,8 +1,9 @@
 package main;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
-public class main {
+public class Main {
 	public static void main(String[] args) {
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
---
+++ Level/src/levelOne.java
@@ -0,0 +1,9 @@
+
+public class levelOne extends level {
+
+	public levelOne() {
+		
+	}
+
+}
+hi cutie
