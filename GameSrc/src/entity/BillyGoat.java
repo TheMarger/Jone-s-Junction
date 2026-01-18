f0244d31a53d6ad80a92e0a244f8c0ee85b5a92b
Tacocaro <Tacocaro@TacoTuesday>
2026-01-18 00:48:29 -0500

    added dialogue

--- GameSrc/src/entity/BillyGoat.java
+++ GameSrc/src/entity/BillyGoat.java
@@ -18,10 +18,11 @@ public class BillyGoat extends entity {
     }
 	
 	public void setDialogues() {
-		dialogues[0] = "44444442444444444444444\n44444444444444444444444\n4444444444444444444444444444\n444444444444444444444444444444";
-		dialogues[1] = "44444444443444444444444\n44444444444444444444444\n4444444444444444444444444444\n444444444444444444444444444444";
-		dialogues[2] = "44444443444444444444444\n44444444444444444444444\n4444444444444444444444444444\n444444444444444444444444444444";
-		dialogues[3] = "44444441444444444444444\n44444444444444444444444\n4444444444444444444444444444\n444444444444444444444444444444";
+		dialogues[0] = "Heh… you think you’re getting out of here?\n Everyone says that on their first day.";
+		dialogues[1] = "There are about four keys scattered around this place: red, yellow, green, and blue.\n Truth is, the blue key is the only one that matters.\n It open the door outside that leads to a van.";
+		dialogues[2] = "And even if you manage to get all the keys, you still have to finish 2 stupid tasks first.";
+		dialogues[3] = "With guards patrolling every hallway.\n One wrong move and you’re done!";
+		dialogues[4] = "But go ahead.\n I’ll still be here when they drag you back...";
 	}
 	
 	public void speak() {
